84419c248cc1feb0d3d9d73ef14866c5e02c3e85
Kirillex22 <ziabrew34@gmail.com>
2024-06-26 23:05:04 +0600

    added character class implementation, created variables with links to images and instances of characters

--- app/src/main/java/com/example/summerpractice2024/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice2024/MainActivity.kt
@@ -13,6 +13,46 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.summerpractice2024.ui.theme.Summerpractice2024Theme
 
+
+class Character(name: String, resourcesLink: String, welcomeMessage: String)
+{
+    private var _name : String = name
+    private var _resourcesLink : String = resourcesLink
+    private var _welcomeMessage = welcomeMessage
+
+    fun getName() : String {
+        return _name
+    }
+
+    fun getLink() : String {
+        return _resourcesLink
+    }
+
+    fun getMessage() : String {
+        return _welcomeMessage
+    }
+}
+
+var MARVER_LOGO_LINK = "https://iili.io/JMnuvbp.png"
+
+var CHARACTERS = listOf(
+    Character(
+        name = "Deadpool",
+        resourcesLink = "https://iili.io/JMnAfIV.png",
+        welcomeMessage = "I'm deadpool, baby!"
+    ),
+    Character(
+        name ="Iron-Man",
+        resourcesLink = "https://iili.io/JMnuDI2.png",
+        welcomeMessage = "I am Iron Man."
+    ),
+    Character(
+        name = "Spider-Man",
+        resourcesLink = "https://iili.io/JMnuyB9.png",
+        welcomeMessage = "Your friendly neighborhood Spider-Man."
+    )
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
